d5b229af34f4709213adac242c20c63814090e82
oowenn <a4shi@ucsd.edu>
2025-02-18 16:36:12 -0800

    lab7 step 1

---
+++ map.js
@@ -0,0 +1,14 @@
+   // Set your Mapbox access token here
+   mapboxgl.accessToken = 'pk.eyJ1IjoiYTRzaGkiLCJhIjoiY203YjVtYXhmMGIwdDJrcHc1Ym5kbHpqciJ9.A3l23yCQealGw-VZ9U-Z7w';
+
+  // Initialize the map
+  const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18 // Maximum allowed zoom
+  });
+
+  
